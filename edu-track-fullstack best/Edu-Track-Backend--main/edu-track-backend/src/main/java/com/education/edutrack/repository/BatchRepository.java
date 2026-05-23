4e1c9c371f25f354b3bb74ccb1a88798249b9110
unknown <madhurapande18@gmail.com>
2026-05-24 01:37:58 +0530

    rename folder and fix Dockerfile

--- edu-track-fullstack best/Edu-Track-Backend--main/edu-track-backend/src/main/java/com/education/edutrack/repository/BatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.education.edutrack.repository;
-
-import com.education.edutrack.entity.BatchEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BatchRepository extends MongoRepository<BatchEntity, String> {
-}
